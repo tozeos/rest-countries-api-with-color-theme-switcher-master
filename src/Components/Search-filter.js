147694271d3f503c1ac5e997847875bb3ca68daa
Caike <caike_tb@ymail.com>
2021-10-18 01:10:15 -0300

    fui dormir

--- src/Components/Search-filter.js
+++ src/Components/Search-filter.js
@@ -4,12 +4,16 @@ import { colors, breakpoints } from "./variables";
 
 export const SearchFilter = () => {
     const Container = styled.div`
-      padding: 0 4.375rem;
+      display: flex;
+      justify-content: space-between;
+      flex-wrap: wrap;
+      padding: 4.375rem 50px 4.375rem 80px;
       background-color: ${({ theme }) => theme.background};
       box-shadow: ${colors.insetShadow};
 
-      @media (max-width: 375px) {
-        padding: 0 30px;
+      @media (max-width: ${breakpoints.mobile}) {
+        padding: 30px 30px;
+        gap: 1rem;
       }
 
       input {
@@ -18,8 +22,6 @@ export const SearchFilter = () => {
         width: 470px;
         height: 55px;
         padding: 0 50px;
-        margin-top: 50px;
-        margin-bottom: 80px;
 
         border: none;
         border-radius: 5px;
@@ -40,9 +42,37 @@ export const SearchFilter = () => {
         }
       }
     `
+
+    const Select = styled.select`
+        background-color: ${({ theme }) => theme.elements};
+        width: 200px;
+        padding: 0 25px;
+        height: 56px;
+        border-radius: 5px;
+        border: none;
+        color: ${({ theme }) => theme.text};
+        box-shadow: ${colors.boxShadow};
+      
+      &::after {
+        padding-right: 10px;
+        
+        option {
+          border: none;
+          // style those
+        }
+      }
+    `
     return (
         <Container>
             <input type="text" placeholder={'Search for a country...'}/>
+            <Select name={'regions'}>
+                <option value={''}>Filter by region</option>
+                <option value={'africa'}>Africa</option>
+                <option value={'america'}>America</option>
+                <option value={'asia'}>Asia</option>
+                <option value={'europe'}>Europe</option>
+                <option value={'oceania'}>Oceania</option>
+            </Select>
         </Container>
     )
 
